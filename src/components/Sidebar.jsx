4b5a855ef2e4c4681af9e73a43f792df237bec81
khalid9996 <khankhalid1743@gmail.com>
2022-06-22 21:04:32 +0530

    Finished Explorer sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,6 +10,14 @@ import {VscLiveShare} from "react-icons/vsc"
 import {HiOutlineUserCircle} from "react-icons/hi"
 import {VscSettingsGear} from "react-icons/vsc"
 import {FiMoreHorizontal} from "react-icons/fi"
+import {MdOutlineExpandMore} from "react-icons/md"
+import {IoIosArrowForward} from "react-icons/io"
+import {BsFolderFill} from "react-icons/bs"
+import {SiJavascript} from "react-icons/si"
+import {FaCss3} from "react-icons/fa"
+import {IoLogoNodejs} from "react-icons/io"
+import {FaGitAlt} from "react-icons/fa"
+import {AiFillExclamationCircle} from "react-icons/ai"
 
 const Sidebar = () => {
   return (
@@ -33,17 +41,29 @@ const Sidebar = () => {
         {/* sidebar explorer */}
         <div className="sidebar__explorer">
             <div className='explorer__txt'><p>Explorer</p><FiMoreHorizontal/></div>
-            <p>>portfolio</p>
-            <p>node_module</p>
-            <p>public</p>
-            <p>src</p>
-            <p>assets</p>
-            <p>>components</p>
-            <p>App.css</p>
-            <p>App.jsx</p>
-            <p>index.css</p>
-            <p>index.js</p>
+            <div className='sidebar__expansion'><MdOutlineExpandMore/><p>portfolio</p></div>
             
+            <div className='explorer_portfolio'>
+            <div className='sidebar__expansion'><IoIosArrowForward/><BsFolderFill color='rgb(93, 204, 93)'/><p>node_module</p></div>
+            <div className='sidebar__expansion'><IoIosArrowForward/><BsFolderFill color='rgb(13, 122, 212)'/><p>public</p></div>
+            <div className='sidebar__expansion'><MdOutlineExpandMore/><BsFolderFill color='rgb(11, 141, 11)'/><p>src</p></div>
+
+            <div className='explorer_src'>
+            <div className='sidebar__expansion'><IoIosArrowForward/><BsFolderFill color='rgb(218, 167, 0)'/><p>assets</p></div>
+            <div className='sidebar__expansion'><MdOutlineExpandMore/><BsFolderFill color='rgb(218, 167, 0)'/><p>components</p></div>
+            
+            <div className='explorer_components'>
+            <div className='sidebar__expansion'><FaCss3 color='rgb(13, 122, 212)'/><p>App.css</p></div>
+            <div className='sidebar__expansion'><SiJavascript color='rgb(218, 167, 0)'/><p>App.js</p></div>
+            <div className='sidebar__expansion'><FaCss3 color='rgb(13, 122, 212)'/><p>index.css</p></div>
+            <div className='sidebar__expansion'><SiJavascript color='rgb(218, 167, 0)'/><p>index.js</p></div>
+            </div>
+            </div>
+            <div className='sidebar__expansion'><FaGitAlt color='rgb(160, 29, 29)'/><p>.gitignore</p></div>
+            <div className='sidebar__expansion'><IoLogoNodejs color='rgb(93, 204, 93)'/><p>pakage-lock.json</p></div>
+            <div className='sidebar__expansion'><IoLogoNodejs color='rgb(93, 204, 93)'/><p>pakage.json</p></div>
+            <div className='sidebar__expansion'><AiFillExclamationCircle color='rgb(0, 176, 235)'/><p>README.md</p></div>
+            </div>
 
         </div>
     </div>
